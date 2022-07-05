0f6870205aa92a22e2123024e933fb0bfde7bb21
Joseph <manueljosephtrabajo@gmail.com>
2022-07-04 18:33:14 -0600

    add the firts file using python

--- firts_file.py
+++ firts_file.py
@@ -48,4 +48,49 @@ price = 49.95
 myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
 print(myorder.format(quantity, itemno, price))
 
+#usando \ para colocar palabras entre comillas
+vh = "Hola joseph Blanco alias \"Mr. White\"."
+print(vh)
+
+#usando booleanos
+print(10 > 20) #False
+print(8 == 5) #False
+print(7 < 1) #True
+
+#usando if y else
+t = 52 + 7
+p = 87 + 2
+
+if p < t :
+    print("hola joseph")
+else :
+    print("hola")
+
+#ejemplo usando una clase para evaluar True or False
+class myclass():
+    def __len__(self):
+        return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+#usando funciones para imprimir True or False
+def myFuntion():
+    return True
+
+print(myFuntion)
+
+#otro ejemplo de lo anterior
+def myFuntion1():
+    return True #False
+
+if myFuntion1():
+    print(◘)
+else :
+    print(♣)
+
+#usando la funcion isinstance(var_name, type)
+u = 200
+print(isinstance(u, int))
+
 #
